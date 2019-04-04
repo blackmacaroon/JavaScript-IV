c8560bb6e562bbfcda9465bb19b5883709a954fa
Kayla <6lackmacaroon@gmail.com>
2019-04-04 13:35:54 -0600

    person and instructor class and methods finished

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,35 @@
 // CODE here for your Lambda Classes
+class Person {
+      constructor(attributes){
+            this.name = attributes.name,
+            this.locat = attributes.locat,
+            this.age = attributes.age,
+            this.gender = attributes.age
+      }
+      speak(){
+            return `Hello, my name is ${this.name}, I am from ${this.locat}!`;
+      }
+};
+
+class Instructor extends Person{
+      constructor(attributes){
+            super(attributes);
+            this.favLang = attributes.favLang,
+            this.specialty = attributes.specialty,
+            this.catchPhrase = attributes.catchPhrase   
+      }
+      demo(subject){
+            return `Today we are learning about ${subject}`;
+      }
+      grade(student, subject){
+            return `${student.name} receives a perfect score on ${subject}`;
+      }
+};
+
+
+
+
+
+
+
+ 
