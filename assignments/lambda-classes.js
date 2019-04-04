6ca18f4fc7abd218ed10c4aeea9a577e4e9b4a0f
Kayla <6lackmacaroon@gmail.com>
2019-04-04 14:36:35 -0600

    seven objects created

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -7,7 +7,7 @@ class Person {
             this.gender = attributes.age
       }
       speak(){
-            return `Hello, my name is ${this.name}, I am from ${this.locat}!`;
+            return `Hello, my name is ${this.name}, I am from ${this.locat}.`;
       }
 };
 
@@ -19,10 +19,10 @@ class Instructor extends Person{
             this.catchPhrase = attributes.catchPhrase   
       }
       demo(subject){
-            return `Today we are learning about ${subject}`;
+            return `Today we are learning about ${subject}.`;
       }
       grade(student, subject){
-            return `${student.name} receives a perfect score on ${subject}`;
+            return `${student.name} receives a perfect score on ${subject}!`;
       }
 };
 
@@ -37,15 +37,97 @@ class Student extends Person {
             return this.favSubjects;
       }
       PRAssignment(subject){
-            return `${this.name} has submitted a PR for ${subject}`;
+            return `${this.name} has submitted a PR for ${subject}.`;
       }
       sprintChallenge(subject){
-            return `${this.name} has begun sprint challenge on ${subject}`;
+            return `${this.name} has begun sprint challenge on ${subject}.`;
+      }
+};
+
+class ProjectManager extends Instructor {
+      constructor(attributes){
+            super(attributes);
+            this.gradeClassName = attributes.gradeClassName,
+            this.favInstructor = attributes.favInstructor
+      }
+      standUp(channel){
+            return `${this.name} announced to ${channel}, @channel standy times!`;
+      }
+      debugsCode(student, subject){
+            return `${this.name} debugs ${student.name}'s code on ${subject}.`;
       }
 };
 
 
 
+const arthur = new Instructor({
+      name: 'Arthur',
+      location: 'Montreal',
+      age: 37,
+      gender: 'male',
+      favLanguage: 'JavaScript',
+      specialty: 'Front-end',
+      catchPhrase: `Hey`
+    });
 
+const buster = new Instructor({
+      name: 'Buster',
+      location: 'Montreal',
+      age: 36,
+      gender: 'male',
+      favLanguage: 'CSS',
+      specialty: 'UI',
+      catchPhrase: `We're just specks of dust floating in space.`
+});
+
+const sueellen = new Instructor({
+      name: 'Sue Ellen',
+      location: 'Pointe-Claire',
+      age: 37,
+      gender: 'female',
+      favLanguage: 'JavaScript',
+      specialty: 'Front-end',
+      catchPhrase: `Animals make better friends than people.`
+});
+
+const binky = new Student({
+      name: 'Binky',
+      location: 'Laval',
+      age: 39,
+      gender: 'male',
+      favLanguage: 'JavaScript',
+      specialty: 'Front-end',
+      catchPhrase: `My dad said he'd buy me a computer when it snows in July`
+    });
+    
+const francine = new Student({
+      name: 'Francine',
+      location: 'Longueuil',
+      age: 37,
+      gender: 'female',
+      favLanguage: 'JavaScript',
+      specialty: 'Front-end',
+      catchPhrase: `Over half the people on Earth are girls`
+});
+
+const muffy = new ProjectManager({
+      name: 'Muffy',
+      location: 'Saint-Eustache',
+      age: 36,
+      gender: 'female',
+      favLanguage: 'JavaScript',
+      specialty: 'Front-end',
+      catchPhrase: `Look ar her, trying to hold her head up high, despite her terrible posture.`
+    });
+    
+const dw = new ProjectManager({
+      name: 'Dora Winifred',
+      location: 'Montreal',
+      age: 30,
+      gender: 'female',
+      favLanguage: 'JavaScript',
+      specialty: 'Front-end',
+      catchPhrase: `I don't care about the president, I care about ponies.`
+});
 
  
